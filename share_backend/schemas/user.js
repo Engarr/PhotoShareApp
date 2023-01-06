01f7e02a89764ea6d95af40b8561688815b17a1b
Łukasz <lukasz.poczta12@gmail.com>
2023-01-06 14:58:00 +0100

    first commit

---
+++ share_backend/schemas/user.js
@@ -0,0 +1,18 @@
+
+export default {
+    name: 'user',
+    type: 'document',
+      title: 'User',
+    fields: [
+      {
+        name: 'userName',
+        type: 'string',
+        title: 'userName'
+      },
+      {
+        name: 'image',
+        type: 'string',
+        title: 'Image'
+      }
+    ]
+  }
